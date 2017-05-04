e765ae8fb40326bb6ae06a4149d9aff4c4335fc3
Gavin Hell <gmangavin1@gmail.com>
2017-05-03 22:28:42 -0700

    Upate 1.2
    Added the ability to click a button.

--- PyWeb.py
+++ PyWeb.py
@@ -5,4 +5,5 @@ import selenium.webdriver #Imports module
  
 driver = selenium.webdriver.Chrome() #Opens chromium browser
 driver.get('https://www.omegle.com') #Goes to this site
+driver.find_element_by_xpath("""//*[@id="textbtn"]""").click() #Click a button.
 print(driver.title) #prints website title to console.
