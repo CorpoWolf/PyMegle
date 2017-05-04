b98f38ba35dcc2dfb09e38ca77911d46f861f9c1
Gavin Hell <gmangavin1@gmail.com>
2017-05-03 21:46:42 -0700

    kk
    kk

--- PyWeb.py
+++ PyWeb.py
@@ -3,4 +3,3 @@ import selenium.webdriver #Imports module
 driver = selenium.webdriver.Chrome() #Opens chromium browser
 driver.get('https://www.omegle.com') #Goes to this site
 print(driver.title) #prints website title to console.
-#test
