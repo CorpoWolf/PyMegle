95893332d5238a28f589adc14f9061cedd253893
Gavin Hell <gmangavin1@gmail.com>
2017-06-17 16:29:03 -0700

    v2.2.0
    Program
    +Can now read messages
    
    PyDoc
    +Notes on reading content from webpage.

---
+++ Program.py
@@ -0,0 +1,47 @@
+'''
+View the coding live on Twitch @ https://www.twitch.tv/gmangavin and look at the github @ https://github.com/gmangavin/PyWeb
+chromedriver for gui view, phantomjs for ghost view.
+'''
+
+import selenium.webdriver #Imports module
+import time #Imports time
+
+N = False #Used for the bool loop.
+while N == False:
+    EngineChoice = input('Would you like a visual of the bot? (Y/N): ') #Part one for the web driver choice
+    YN = (EngineChoice.lower()) #Prevents capatalization error.
+    if YN == ('y'):
+        while N == False:
+            VarChoice = input('Would you like Firefox or Chrome?: (F/C)') #Part two for the web driver choice
+            FC = (VarChoice.lower()) #Prevents capatalization error.
+            if FC == ('f'):
+                WebVar = selenium.webdriver.Firefox()
+                N = True
+            elif FC == ('c'):
+                WebVar = selenium.webdriver.Chrome()
+                N = True
+            else:
+                print('Try again')
+    elif YN == ('n'):
+        WebVar = selenium.webdriver.PhantomJS()
+        N = True
+    else:
+        print('Try again')
+#A while loop to make sure the user enters in a correct character.
+#Allows the user to choose which web driver they want to use.
+
+Interest = input("What is a common interest you're looking for?: ")
+WebVar.get('https://www.omegle.com')
+print(WebVar.title)
+
+WebVar.find_element_by_xpath('//*[@id="topicsettingscontainer"]/div/div[1]/span[2]').click() #Clicks the area for typing
+Send = WebVar.find_element_by_xpath('//*[@id="topicsettingscontainer"]/div/div[1]/span[2]') #Creates an input variable for text area.
+Send.send_keys(Interest + ',') #Sends input to text area.
+
+WebVar.find_element_by_xpath('//*[@id="textbtn"]').click() #Clicks the 'text' button
+
+Connected = False
+while Connected == False:
+    Status = WebVar.find_element_by_xpath('/html/body/div[7]/div/div/div[1]/div[1]/div').text #Takes the text info from xpath.
+    print(Status)
+    time.sleep(1) #Waits one second to continue.
---
+++ PyDoc.py
@@ -0,0 +1,47 @@
+﻿'''
+    Reason to use Selenium over the webscrape module "urllib.request" is because Selenium uses web engine which can render JavaScript webpages.
+    More notes on my publically viewable google doc, https://www.bit.ly/JeePython
+'''
+import time
+
+def Delay():
+    time.sleep(1)
+#Sometimes you're going to need to let the JavaScript load, easily done by adding a second delay.
+
+import selenium.webdriver
+#Selenium is a 3rd party module installed through the command prompt by typing "pip install selenium"
+#Selenium.webdriver is what I call a "sub-module" which is installed with selenium
+
+WebVar = selenium.webdriver.Chrome()
+#WebVar is the variable we assigned to selenium.webdrive.Chrome
+#Chrome is the web engine being used and needs to be installed from https://sites.google.com/a/chromium.org/chromedriver/downloads
+
+WebVar2 = selenium.webdriver.PhantomJS()
+#Another example using a seperate web engine called PhatomJS which can be installed from http://phantomjs.org/
+#PhantomJS unlike Chromedriver is a "HEADLESS" webbrowser meaning it will not display anything.
+#Chrome is developing a headless webbrowser of their own now.
+
+WebVar.get('https://website.com')
+#This is function that can recieve information from a website.
+
+print(WebVar.title)
+#This will print the title header of a website that our variable has obtained.
+
+WebVar.find_element_by_xpath('//*[@id="site-menu"]/li[2]/a').click()
+WebVar.find_element_by_xpath('//*[@id="designSideBar"]/span').click()
+#WebVar.find_element_by_xpath('//*[@id="searchTemplates"]').click()
+#This allows you to have selenium automatically click something for you.
+#Firstly, You need to get the xpath name, you obtain this through the inspection tool on your web browser.
+#Right click the button > Click inspect > Right click highlighted element > Copy > Copy Xpath
+
+Delay()
+#Here we call a delay to prevent an error of Xpath not found.
+
+print(WebVar.find_element_by_xpath('//*[@id="site-body"]/div[1]/p').text)
+#This function allows the website text to be printed.
+
+WebVar.find_element_by_xpath('//*[@id="searchTemplates"]/form/input[1]').click()
+
+Send = WebVar.find_element_by_xpath('//*[@id="searchTemplates"]/form/input[1]')
+Send.send_keys('test')
+#This allows us to put text input into fields that can take text such as a search bar.
