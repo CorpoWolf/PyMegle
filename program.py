0037481663ca3bb76bbebeef706514053f2a208a
BlackTiger1500 <alihasnain09@hotmail.com>
2017-06-18 18:06:23 -0400

    Added message stream

--- program.py
+++ program.py
@@ -7,26 +7,27 @@ import selenium.webdriver #Imports module
 import time #Imports time
 
 N = False #Used for the bool loop.
-while N == False:
+while not N:
     EngineChoice = input('Would you like a visual of the bot? (Y/N): ')#Part one for the web driver choice
     YN = (EngineChoice.lower())#Prevents capatalization error.
-    if YN == ('y'):
-        while N == False:
-            VarChoice = input('Would you like Firefox or Chrome?: (F/C)')#Part two for the web driver choice
+    if YN == 'y':
+        while not N:
+            VarChoice = input('Would you like Firefox or Chrome? (F/C): ')#Part two for the web driver choice
             FC = (VarChoice.lower())#Prevents capatalization error.
-            if FC == ('f'):
+            if FC == 'f':
                 WebVar = selenium.webdriver.Firefox()
-                N = True
-            elif FC == ('c'):
+            elif FC == 'c':
                 WebVar = selenium.webdriver.Chrome()
-                N = True
             else:
                 print('Try again')
-    elif YN == ('n'):
+                continue
+            N = True
+    elif YN == 'n':
         WebVar = selenium.webdriver.PhantomJS()
-        N = True
     else:
         print('Try again')
+        continue
+    N = True
 #A while loop to make sure the user enters in a correct character.
 #Allows the user to choose which web driver they want to use.
 
@@ -34,8 +35,14 @@ Interest = input("What is a common interest you're looking for?: ")
 WebVar.get('https://www.omegle.com')
 print(WebVar.title)
 
-WebVar.find_element_by_xpath('//*[@id="topicsettingscontainer"]/div/div[1]/span[2]').click()#Clicks the area for typing
-Send = WebVar.find_element_by_xpath('//*[@id="topicsettingscontainer"]/div/div[1]/span[2]')#Creates an input variable for text area.
-Send.send_keys(Interest + ',')#Sends input to text area.
+interestBox = WebVar.find_element_by_xpath('//*[@id="topicsettingscontainer"]/div/div[1]/span[2]')#Creates an input variable for text area.
+interestBox.send_keys(Interest + ',')#Sends input to text area.
 
 WebVar.find_element_by_xpath('//*[@id="textbtn"]').click()#Clicks the 'text' button
+lastMessage = None
+
+while True:
+    content = WebVar.find_element_by_xpath('/html/body/div[7]/div/div/div[1]/div[1]/div').text
+    if content != lastMessage:
+        print(content)
+        lastMessage = content
