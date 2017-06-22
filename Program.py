cfe28b5401fbd1a27fef16791e0e03b617f9fb4d
Gavin Hell <gmangavin1@gmail.com>
2017-06-21 20:54:23 -0700

    v2.6.0
    Program:
    +Added command /end which disconnects
    +Added command /start which RECONNECTS
    
    ~+Added bug with /change [Completely broken]

--- Program.py
+++ Program.py
@@ -7,6 +7,7 @@ import selenium.webdriver #Imports module
 import time #Imports time
 import threading #Imports threading, used to have multiple things happen at the same time.
 import os #Imports OS
+import sys
 
 N = False #Used for the bool loop.
 while N == False:
@@ -21,13 +22,13 @@ while N == False:
                     WebVar = selenium.webdriver.Firefox()
                     N = True
                 except selenium.common.exceptions.WebDriverException:
-                    print("You don't seem to have this webdriver installed.")
+                    print("You don't seem to have the firefox webdriver installed. You can get it here https://github.com/mozilla/geckodriver/releases")
             elif FC == ('c'):
                 try:
                     WebVar = selenium.webdriver.Chrome()
                     N = True
                 except:
-                    print("You don't seem to have this webdriver installed.")
+                    print("You don't seem to have the chrome webdriver installed. You can get it here https://sites.google.com/a/chromium.org/chromedriver/downloads")
             else:
                 print('Try again')
     elif YN == ('n'):
@@ -35,7 +36,7 @@ while N == False:
             WebVar = selenium.webdriver.PhantomJS()
             N = True
         except selenium.common.exceptions.WebDriverException:
-                    print("You don't seem to have this webdriver installed.")
+                    print("You don't seem to have the PhantomJS webdriver installed. You can get it here http://phantomjs.org/")
     else:
         print('Try again')
 #A while loop to make sure the user enters in a correct character.
@@ -51,24 +52,68 @@ Send.send_keys(Interest + ',') #Sends input to text area.
 
 WebVar.find_element_by_xpath('//*[@id="textbtn"]').click() #Clicks the 'text' button
 
+Disconnected = False
+
+def Disconnect(*args):
+    WebVar.find_element_by_xpath('/html/body/div[7]/div/div/div[2]/table/tbody/tr/td[1]/div/button').click()
+    WebVar.find_element_by_xpath('/html/body/div[7]/div/div/div[2]/table/tbody/tr/td[1]/div/button').click()
+    global Disconnected
+    Disconnected = True
+    return Disconnected
+
+def Change(*args):
+    if Disconnected == True:
+        os.system('cls')
+        WebVar.find_element_by_xpath('/html/body/div[7]/div/div/div[1]/div[1]/div/div[4]/div/a').click()
+        Interest = input("What is a common interest you're looking for?: ")
+        Send2 = WebVar.find_element_by_class_name('topicplaceholder')
+        Send2.send_keys(Interest + ',') #Sends input to text area.
+    else:
+        print("You need to disconnect first")
+
+def Connect(*args):
+    if Disconnected == True:
+        os.system('cls')
+        WebVar.find_element_by_xpath('/html/body/div[7]/div/div/div[2]/table/tbody/tr/td[1]/div/button').click()
+        os.system('cls')
+        print('Rebooting search.')
+        threading.Thread(target=StatusMode)._stop()
+        threading.Thread(target=UserMode)._stop()
+        time.sleep(1)
+        threading.Thread(target=StatusMode).start()
+    elif Disconnected == False:
+        print("You're still connected.")
+    else:
+        print("something is just broken")
+
 def UserMode(*args):
     while True:
         UserM = input('') #Has the user type an interest.
-        Sending = WebVar.find_element_by_class_name('chatmsg') #Takes the class used for user input.
-        Sending.send_keys(UserM)
-        WebVar.find_element_by_xpath('/html/body/div[7]/div/div/div[2]/table/tbody/tr/td[3]/div/button').click()
+        if UserM == "/end":
+            Disconnect()
+        elif UserM == "/start":
+            Connect()
+        elif UserM == "/change":
+            Change()
+        else:
+            Sending = WebVar.find_element_by_class_name('chatmsg') #Takes the class used for user input.
+            Sending.send_keys(UserM)
+            WebVar.find_element_by_class_name('sendbtn').click()
 
 def StatusMode(*args):
-    threading.Thread(target=UserMode).start()
-    StatusNew = None
+    threading.Thread(target=UserMode).start() #Starts the operation in a thread.
+    StatusNew = None #Create a variable with no value.
     while True:
         Status = WebVar.find_element_by_xpath('/html/body/div[7]/div/div/div[1]/div[1]/div').text #Takes the text info from xpath
         if StatusNew == (Status):
             continue
         else:
             StatusNew = Status
-            os.system('cls')#Refreshes chat.
-            print(StatusNew)
-            print('')
+            if "Stranger has disconnected." not in Status:
+                os.system('cls') #Refreshes chat.
+                print(StatusNew)
+                print('')
+            else:
+                Disconnect()
 
-threading.Thread(target=StatusMode).start()
+threading.Thread(target=StatusMode).start() #Starts the operation in a thread.
